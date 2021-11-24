023639b4d7d391727a8c5251e1d3a7ee2b06244a
dragonhyeon <kyh.mv@movements.kr>
2021-11-24 15:53:52 +0900

    1: initial version

---
+++ calculate.js
@@ -0,0 +1,10 @@
+function add(age, weight) {
+
+    let tubeSize = age / 4 + 3.5
+    let Robi = weight / 300
+    let PFOL = weight * 2
+    let Rocu = weight / 2
+    let Reverse = weight / 150 - weight / 4
+
+    alert('Tube size: ' + tubeSize + '\nRobi: ' + Robi + '\nPFOL: ' + PFOL + '\nRocu' + Rocu + '\nReverse(Robi-PSTG)' + Reverse)
+}
