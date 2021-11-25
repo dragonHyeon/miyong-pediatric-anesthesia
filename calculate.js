5e15fd849f7ce0d42820263fa7409e397da7fca3
dragonhyeon <kyh.mv@movements.kr>
2021-11-25 18:05:21 +0900

    2: updated

--- calculate.js
+++ calculate.js
@@ -1,10 +1,10 @@
 function add(age, weight) {
 
-    let tubeSize = age / 4 + 3.5
-    let Robi = weight / 300
-    let PFOL = weight * 2
-    let Rocu = weight / 2
-    let Reverse = weight / 150 - weight / 4
+    let tubeSize = (age / 4 + 3.5).toFixed(2)
+    let Robi = (weight / 300).toFixed(2)
+    let PFOL = (weight * 2).toFixed(2)
+    let Rocu = (weight / 2).toFixed(2)
+    let Reverse = [(weight / 150).toFixed(2), (weight / 4).toFixed(2)]
 
-    alert('Tube size: ' + tubeSize + '\nRobi: ' + Robi + '\nPFOL: ' + PFOL + '\nRocu' + Rocu + '\nReverse(Robi-PSTG)' + Reverse)
+    alert('Tube size: ' + tubeSize + '\nRobi: ' + Robi + '\nPFOL: ' + PFOL + '\nRocu: ' + Rocu + '\nReverse(Robi/PSTG): ' + Reverse[0] + '/' + Reverse[1])
 }
